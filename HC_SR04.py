237d183eb665ffc4624c0eac11264eac7a2127e5
Denis Immoos <denisimmoos@gmail.com>
2021-01-24 01:58:38 +0100

    Cleaning upsome Class stuff

--- HC_SR04.py
+++ HC_SR04.py
@@ -1,7 +1,11 @@
 
 class Distance(object):
 
-    """docstring for HC-SR04."""
+    """HC-SR04 - At the moment you can mesure a distance 
+       :) and in the future not more"""
+
+    import RPi.GPIO as GPIO
+    import time
 
     def __init__(self, GPIO_TRIGGER, GPIO_ECHO):
 
@@ -10,33 +14,30 @@ class Distance(object):
 
     def distance(self):
 
-        import RPi.GPIO as GPIO
-        import time
-
         # GPIO Mode (BOARD / BCM)
-        GPIO.setmode(GPIO.BCM)
+        self.GPIO.setmode(self.GPIO.BCM)
 
         # set GPIO direction (IN / OUT)
-        GPIO.setup(self.GPIO_TRIGGER, GPIO.OUT)
-        GPIO.setup(self.GPIO_ECHO, GPIO.IN)
+        self.GPIO.setup(self.GPIO_TRIGGER, self.GPIO.OUT)
+        self.GPIO.setup(self.GPIO_ECHO, self.GPIO.IN)
 
-        time.sleep(0.01)
+        self.time.sleep(0.01)
 
         # set Trigger to HIGH
-        GPIO.output(self.GPIO_TRIGGER, True)
+        self.GPIO.output(self.GPIO_TRIGGER, True)
 
         # set Trigger after 0.01ms to LOW
         #time.sleep(0.00001)
-        time.sleep(0.01)
-        GPIO.output(self.GPIO_TRIGGER, False)
+        self.time.sleep(0.01)
+        self.GPIO.output(self.GPIO_TRIGGER, False)
 
-        StartTime = time.time()
-        StopTime = time.time()
+        StartTime = self.time.time()
+        StopTime = self.time.time()
 
         # save StartTime
-        ReadTime = time.time()
-        while GPIO.input(self.GPIO_ECHO) == 0:
-            StartTime = time.time()
+        ReadTime = self.time.time()
+        while self.GPIO.input(self.GPIO_ECHO) == 0:
+            StartTime = self.time.time()
 
 	    # return 9999.9 if we got stuck
             if ReadTime - StartTime > 0.2:
@@ -44,9 +45,9 @@ class Distance(object):
               break
 
         # save time of arrival
-        ReadTime = time.time()
-        while GPIO.input(self.GPIO_ECHO) == 1:
-            StopTime = time.time()
+        ReadTime = self.time.time()
+        while self.GPIO.input(self.GPIO_ECHO) == 1:
+            StopTime = self.time.time()
 
 	    # return 9999.9 if we got stuck
             if ReadTime - StopTime > 0.2:
@@ -59,6 +60,6 @@ class Distance(object):
         # and divide by 2, because there and back
         distincm = (TimeElapsed * 34300) / 2
 
-        GPIO.cleanup()
+        self.GPIO.cleanup()
 
         return distincm
